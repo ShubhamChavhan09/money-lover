24ae0f1a51d082bc65e8e99fd89451b165ecd5e8
Shubham <shubh.chavhan00@gmail.com>
2022-02-16 17:29:13 +0530

    added background color to body

--- src/global-styles/index.js
+++ src/global-styles/index.js
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-* {
-    box-sizing: border-box
+*
+ {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
 }
 
 body{
-    margin: 0;
-    padding: 0;
+    background: #ecf0f1;  
 }
 `;
 
