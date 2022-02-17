91336b22c41b36b7960b5b47f533579291e88248
Shubham <shubh.chavhan00@gmail.com>
2022-02-17 22:59:46 +0530

    added budget card component

--- src/components/budget-card/index.js
+++ src/components/budget-card/index.js
@@ -1,21 +1,75 @@
 import React from "react";
 import styled from "styled-components";
+import { useBudgets } from "../../context";
 
 const BudgetCard = () => {
+  const { budgets } = useBudgets();
+
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
   return (
-    <Contanier>
-      <h1>Budget Card</h1>
-      <div>
-        <button>Add Expense</button>
-      </div>
-    </Contanier>
+    <>
+      {budgets.map((budget) => {
+        return (
+          <Card key={budget.id}>
+            <div>
+              <h2>{budget.name}</h2>
+              <span>Amount / {budget.max}</span>
+            </div>
+            <progress value="50" max="100" />
+            <div>
+              <button onClick={() => handleClick(budget.id)}>
+                Add Expense
+              </button>
+              <button>View Expenses</button>
+            </div>
+          </Card>
+        );
+      })}
+    </>
   );
 };
 
 export default BudgetCard;
 
-const Contanier = styled.div`
+const Card = styled.div`
   text-align: left;
   border: 2px solid green;
+  border-radius: 8px;
   height: 180px;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+  padding: 10px;
+  margin: 0 auto;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+  button {
+    border-radius: 8px;
+    outline: none;
+    border: none;
+    background: #81ecec;
+    padding: 8px 12px;
+    width: 120px;
+  }
+  button:hover {
+    background: #00cec9;
+    transition: all 0.1s linear;
+  }
+  button: active {
+    background: #55efc4;
+    transition: all 0.1s linear;
+  }
+  progress {
+    width: 60%;
+    height: 30px;
+    margin: 0 auto;
+  }
 `;
