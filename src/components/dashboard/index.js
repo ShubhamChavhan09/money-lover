978bfee6ea1f8bf676abe6647c38118b8afd60db
Shubham <shubh.chavhan00@gmail.com>
2022-02-17 14:10:33 +0530

    added toggle function for the modal

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,18 +1,29 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import BudgetCard from "../budget-card";
+import BudgetModal from "../budget-modal";
 
 const Dashboard = () => {
+  const [budgetModal, setBudgetModal] = useState(false);
+
+  const handleBudget = () => {
+    setBudgetModal(!budgetModal);
+  };
+
   return (
-    <Container>
-      <h1>Money Lover</h1>
-      <div>
-        <button>Add Budget</button>
-        <button>Add Expense</button>
-      </div>
-      <div className="budget">
-        <BudgetCard />
-      </div>
-    </Container>
+    <>
+      <Container>
+        <h1>Money Lover</h1>
+        <div>
+          <button onClick={handleBudget}>Add Budget</button>
+          <button>Add Expense</button>
+        </div>
+        <div className="budget">
+          <BudgetCard />
+        </div>
+      </Container>
+      <BudgetModal handleBudget={handleBudget} budgetModal={budgetModal} />
+    </>
   );
 };
 
@@ -26,6 +37,7 @@ const Container = styled.div`
   border: 1px solid black;
   width: 60%;
   min-height: 100vh;
+  position: relative;
 
   button {
     margin: 0 10px;
