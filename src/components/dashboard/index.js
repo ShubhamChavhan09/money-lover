9c5e1f432eeb089ebb045e265ab8e08e6bffa157
Shubham <shubh.chavhan00@gmail.com>
2022-02-18 23:07:03 +0530

    added prop to the expense modal

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,27 +2,41 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import BudgetCard from "../budget-card";
 import BudgetModal from "../budget-modal";
+import ExpenseModal from "../expense-modal";
 
 const Dashboard = () => {
   const [budgetModal, setBudgetModal] = useState(false);
+  const [expenseModal, setExpenseModal] = useState(false);
+  const [expenseBudgetId, setExpenseBudgetId] = useState();
 
   const handleBudget = () => {
     setBudgetModal(!budgetModal);
   };
 
+  const handleExpense = (budgetId) => {
+    setExpenseModal(!expenseModal);
+    setExpenseBudgetId(budgetId);
+    console.log(budgetId);
+  };
+
   return (
     <>
-      <Container>
+      <Container budgetModal={budgetModal} expenseModal={expenseModal}>
         <h1>Money Lover</h1>
         <div>
           <button onClick={handleBudget}>Add Budget</button>
-          <button>Add Expense</button>
+          <button onClick={handleExpense}>Add Expense</button>
         </div>
         <div className="budget">
-          <BudgetCard />
+          <BudgetCard handleExpense={handleExpense} />
         </div>
       </Container>
       <BudgetModal handleBudget={handleBudget} budgetModal={budgetModal} />
+      <ExpenseModal
+        handleExpense={handleExpense}
+        expenseModal={expenseModal}
+        expenseBudgetId={expenseBudgetId}
+      />
     </>
   );
 };
@@ -35,9 +49,12 @@ const Container = styled.div`
   justify-content: flex-start;
   align-items: center;
   border: 1px solid black;
-  width: 60%;
+  width: 100vw;
   min-height: 100vh;
   position: relative;
+  filter: ${(props) => (props.budgetModal ? `blur(2px)` : null)};
+  filter: ${(props) => (props.expenseModal ? `blur(2px)` : null)};
+  transition: all 0.3s ease;
 
   button {
     margin: 0 10px;
