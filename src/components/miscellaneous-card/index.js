78554c24fe8c055524058cd675117021b5f03f6a
Shubham <shubh.chavhan00@gmail.com>
2022-02-23 13:12:31 +0530

    added miscellaneous expense card

---
+++ src/components/miscellaneous-card/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { MISCELLANEOUS_BUDGET_ID, useBudgets } from "../../context";
+import BudgetCard from "../budget-card";
+
+const MiscellaneousCard = (props) => {
+  const { getBudgetExpenses } = useBudgets();
+
+  const amount = getBudgetExpenses(MISCELLANEOUS_BUDGET_ID).reduce(
+    (total, expense) => total + expense.amount,
+    0
+  );
+
+  if (amount === 0) return null;
+
+  return (
+    <div>
+      <BudgetCard
+        {...props}
+        name="Miscellaneous"
+        id={MISCELLANEOUS_BUDGET_ID}
+        amount={amount}
+      />
+    </div>
+  );
+};
+
+export default MiscellaneousCard;
