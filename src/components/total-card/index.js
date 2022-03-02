5ac0cab8bd85fa036c6ab01bfb53b736f9755404
Shubham <shubh.chavhan00@gmail.com>
2022-03-02 10:21:51 +0530

    added component to show all the expenses and budgets total

---
+++ src/components/total-card/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import BudgetCard from "../budget-card";
+import { useBudgets } from "../../context";
+
+const TotalCard = () => {
+  const { getBudgetExpenses, expenses, budgets } = useBudgets();
+  const amount = expenses.reduce((total, expense) => total + expense.amount, 0);
+
+  const max = budgets.reduce((total, budget) => total + budget.max, 0);
+
+  if (amount === 0) return null;
+
+  return <BudgetCard name="Total" amount={amount} max={max} hidden />;
+};
+
+export default TotalCard;
