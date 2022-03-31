6caa00e89950cf07337ed7f86c76c3acc6c45519
Shubham <shubh.chavhan00@gmail.com>
2022-03-31 21:28:26 +0530

    added function to change color of the progress bar based on the ratio

--- src/components/budget-card/index.js
+++ src/components/budget-card/index.js
@@ -13,16 +13,30 @@ const BudgetCard = ({
 }) => {
   const rate = (amount / max) * 100;
 
+  function setProgressVariant(amount, max) {
+    const ratio = amount / max;
+    if (ratio < 0.6) return "#44bd32";
+    if (ratio < 0.8) return "#fbc531";
+    return "#d63031";
+  }
+
+  const classNames = [];
+  if (amount > max) {
+    classNames.push("danger");
+  }
+
   return (
     <>
-      <Card>
+      <Card className={classNames}>
         <div>
           <h2>{name}</h2>
           <span>
             {amount} {(max || max === `0`) && `/ ${max}`}
           </span>
         </div>
-        {(max || max === `0`) && <progress value={rate} max="100" />}
+        <ProgressBar col={setProgressVariant(amount, max)}>
+          {(max || max === `0`) && <progress value={rate} max="100" />}
+        </ProgressBar>
         {!hidden && (
           <div>
             <button onClick={() => handleExpense(id)}>Add Expense</button>
@@ -48,6 +62,7 @@ const Card = styled.div`
   align-items: stretch;
   padding: 10px;
   margin: 0 auto;
+  transition: all 0.5s ease;
 
   div {
     display: flex;
@@ -66,13 +81,37 @@ const Card = styled.div`
     background: #00cec9;
     transition: all 0.1s linear;
   }
-  button: active {
+  button:active {
     background: #55efc4;
-    transition: all 0.1s linear;
+    transition: all 0.1s ease-in-out;
   }
+  &.danger {
+    background-color: rgba(255, 82, 82, 0.3);
+  }
+`;
+
+const ProgressBar = styled.div`
+  color: #e67e22;
+
   progress {
-    width: 60%;
-    height: 30px;
+    width: 90%;
     margin: 0 auto;
+  }
+
+  progress[value] {
+    appearance: none;
+
+    ::-webkit-progress-bar {
+      height: 10px;
+      border-radius: 20px;
+      background-color: #777;
+    }
+
+    ::-webkit-progress-value {
+      height: 10px;
+      border-radius: ${(props) => (props.col === "#d63031" ? "20px" : null)};
+      background-color: ${(props) => props.col};
+      transition: all 1s linear;
+    }
   }
 `;
