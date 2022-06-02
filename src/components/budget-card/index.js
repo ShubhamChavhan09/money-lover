84b92e4bbfb1e936e40cc08025815dccd94b2f3e
Shubham <shubh.chavhan00@gmail.com>
2022-06-02 06:55:41 +0530

    changed layout of the home page

--- src/components/budget-card/index.js
+++ src/components/budget-card/index.js
@@ -1,25 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { useBudgets } from "../../context";
 import { currencyFormatter } from "../../utils";
 
-const BudgetCard = ({
-  handleExpense,
-  handleViewExpense,
-  amount,
-  name,
-  max,
-  id,
-  hidden,
-}) => {
+const BudgetCard = ({ handleExpense, amount, name, max, id, hidden }) => {
   const rate = (amount / max) * 100;
 
   function setProgressVariant(amount, max) {
     const ratio = amount / max;
-    if (ratio < 0.6) return "#44bd32";
-    if (ratio < 0.8) return "#fbc531";
-    return "#d63031";
+    if (ratio < 0.6) return "rgba(76, 209, 55,0.75)";
+    if (ratio < 0.8) return "rgba(255, 211, 42,0.75)";
+    return "rgba(232, 65, 24,0.7)";
   }
 
   const classNames = [];
@@ -30,26 +21,43 @@ const BudgetCard = ({
   return (
     <>
       <Card className={classNames}>
-        <div>
-          <h2>{name}</h2>
-          <span>
-            {currencyFormatter.format(amount)}
-
-            {(max || max === `0`) && ` / ${currencyFormatter.format(max)}`}
-          </span>
-        </div>
-        <ProgressBar col={setProgressVariant(amount, max)}>
-          {(max || max === `0`) && <progress value={rate} max="100" />}
-        </ProgressBar>
-        {!hidden && (
+        <h3>{name}</h3>
+        {(max || max === `0`) && (
           <div>
-            <button onClick={() => handleExpense(id)}>Add Expense</button>
-            <button onClick={() => handleViewExpense(id)}>View Expenses</button>
-            {/* <Link to={`expenses/${name}`}>
-              <button>View Expenses</button>
-            </Link> */}
+            <p> {currencyFormatter.format(max)}</p>
+            <ProgressBar variant={setProgressVariant(amount, max)}>
+              <progress value={rate} max="100" />
+            </ProgressBar>
           </div>
         )}
+
+        <Data>
+          <div>
+            <span>Spent</span>
+            <span>{currencyFormatter.format(amount)}</span>
+          </div>
+          {(max || max === `0`) && (
+            <div>
+              <span>{rate >= 100 ? "Over Budget" : "Left"}</span>
+              <span>{currencyFormatter.format(max - amount)}</span>
+            </div>
+          )}
+        </Data>
+        <Buttons>
+          <div>
+            {!hidden && (
+              <button onClick={() => handleExpense(id)}>Add Expense</button>
+            )}
+          </div>
+          <div>
+            {!hidden && (
+              // <button onClick={() => handleViewExpense(id)}>View Expenses</button>
+              <Link to={`expenses/${id}`}>
+                <button>View Expenses</button>
+              </Link>
+            )}
+          </div>
+        </Buttons>
       </Card>
     </>
   );
@@ -58,31 +66,90 @@ const BudgetCard = ({
 export default BudgetCard;
 
 const Card = styled.div`
-  text-align: left;
-  border: 2px solid green;
+  // border: 2px solid #718093;
   border-radius: 8px;
-  height: 180px;
-  width: 280px;
+  height: 200px;
+  width: 320px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  align-items: stretch;
-  padding: 10px;
-  margin: 0 auto;
+  padding: 10px 20px;
   transition: all 0.5s ease;
+  background: rgba(178, 190, 195, 0.2);
 
-  div {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  h3,
+  p {
+    text-align: center;
   }
+
+  &.danger {
+    background-color: rgba(255, 127, 80, 0.3);
+  }
+`;
+
+const ProgressBar = styled.div`
+  color: #e67e22;
+  display: flex;
+
+  progress {
+    width: 100%;
+  }
+
+  progress[value] {
+    appearance: none;
+
+    ::-webkit-progress-bar {
+      height: 25px;
+      border-radius: 5px;
+      background-color: rgba(45, 52, 54, 0.8);
+      outline: 1px solid rgba(30, 39, 46, 1);
+    }
+
+    ::-webkit-progress-value {
+      height: 25px;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+      border-top-right-radius: ${(props) =>
+        props.variant === "rgba(232, 65, 24,0.7)" ? "5px" : "0"};
+      border-bottom-right-radius: ${(props) =>
+        props.variant === "rgba(232, 65, 24,0.7)" ? "5px" : "0"};
+      background-color: ${(props) => props.variant};
+      transition: all 0.5s linear;
+    }
+  }
+`;
+
+const Data = styled.div`
+  // background: pink;
+  margin: 20px 0 0 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    flex: 1;
+  }
+  & > div:nth-child(2) {
+    border-left: 1.3px solid #34495e;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+
   button {
-    border-radius: 8px;
+    border-radius: 5px;
     outline: none;
     border: none;
     background: #81ecec;
-    padding: 8px 12px;
+    // padding: 8px 12px;
     width: 120px;
+    font-size: 12px;
   }
   button:hover {
     background: #00cec9;
@@ -91,34 +158,5 @@ const Card = styled.div`
   button:active {
     background: #55efc4;
     transition: all 0.1s ease-in-out;
-  }
-  &.danger {
-    background-color: rgba(255, 82, 82, 0.3);
-  }
-`;
-
-const ProgressBar = styled.div`
-  color: #e67e22;
-
-  progress {
-    width: 90%;
-    margin: 0 auto;
-  }
-
-  progress[value] {
-    appearance: none;
-
-    ::-webkit-progress-bar {
-      height: 10px;
-      border-radius: 20px;
-      background-color: #777;
-    }
-
-    ::-webkit-progress-value {
-      height: 10px;
-      border-radius: ${(props) => (props.col === "#d63031" ? "20px" : null)};
-      background-color: ${(props) => props.col};
-      transition: all 1s linear;
-    }
   }
 `;
