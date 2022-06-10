b205d04f16a42a1f51c984478c382ba8d8e4cc32
Shubham <shubh.chavhan00@gmail.com>
2022-06-10 18:20:39 +0530

    changed layout of budgetCard

--- src/components/budget-modal/index.js
+++ src/components/budget-modal/index.js
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 import { useBudgets } from "../../context";
 import { CgClose } from "react-icons/cg";
+import { Delete } from "../view-expense";
+import ModalButtons from "../modal-buttons";
 
-const BudgetModal = ({ handleBudget, budgetModal }) => {
+const BudgetModal = ({ budgetModal, handleBudget }) => {
   const nameRef = useRef("");
   const maxRef = useRef("");
   const modalRef = useRef("");
@@ -34,25 +36,41 @@ const BudgetModal = ({ handleBudget, budgetModal }) => {
   return (
     <>
       {budgetModal ? (
-        <Overlay ref={modalRef} onClick={close} width="400px" height="400px">
+        <Overlay ref={modalRef} onClick={close} width="350px" height="390px">
           <div className="modal">
-            <CloseModal onClick={() => handleBudget()} />
-            <h2>Add a new budget</h2>
-            <section>
-              <Form onSubmit={handleSubmit}>
-                <div>
-                  <label>Name</label>
-                  <input type="text" required ref={nameRef} />
-                </div>
-                <div>
-                  <label>Amount</label>
-                  <input type="number" required min="0" ref={maxRef} />
-                </div>
-                <div>
-                  <Button type="submit">Add Budget</Button>
-                </div>
-              </Form>
-            </section>
+            {/* <CloseModal  /> */}
+            <Title>
+              <p>Add Budget</p>
+            </Title>
+            <Form section="240px" onSubmit={handleSubmit}>
+              <section>
+                <Box>
+                  <div>
+                    <p>Name</p>
+                  </div>
+                  <div>
+                    <input required type="text" ref={nameRef} />
+                  </div>
+                </Box>
+                <Box>
+                  <div>
+                    <p>Amount</p>
+                  </div>
+                  <div>
+                    <input type="number" min="0" ref={maxRef} />
+                  </div>
+                </Box>
+                <Box>
+                  <div>
+                    <p>Date</p>
+                  </div>
+                  <div>
+                    <input type="date" placeholder="select" />
+                  </div>
+                </Box>
+              </section>
+              <ModalButtons cancel={handleBudget} />
+            </Form>
           </div>
         </Overlay>
       ) : null}
@@ -63,7 +81,7 @@ const BudgetModal = ({ handleBudget, budgetModal }) => {
 export default BudgetModal;
 export const Overlay = styled.div`
   position: fixed;
-  z-index: 1;
+  z-index: 10;
   display: flex;
   inset: 0;
   background: rgba(0, 0, 0, 0.5);
@@ -71,18 +89,27 @@ export const Overlay = styled.div`
   div.modal {
     color: black;
     margin: auto;
-    background: #fff;
+    background: #ffffff;
     height: ${(props) => props.height};
     width: ${(props) => props.width};
-    position: relative;
-    padding: 40px 25px;
-    border-radius: 5px;
-    background: linear-gradient(145deg, #ffffff, #e6e6e6);
+    // position: relative;
+    border-radius: 4px;
+
     // box-shadow: 35px 35px 70px #a8a8a8, -35px -35px 70px #ffffff;
     // z-index: 2;
-    h2 {
-      margin-bottom: 10px;
-    }
+  }
+
+  section {
+    padding: 24px 24px 0;
+    font-size: 14px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    // flex-wrap: wrap;
+    // gap: 10px;
+    //
+    height: 100%;
   }
 `;
 
@@ -96,38 +123,48 @@ export const CloseModal = styled(CgClose)`
 `;
 
 export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-
-  & > div {
-    display: flex;
-    flex-direction: column;
-    margin: 15px 0;
-    font-size: 17px;
-    font-weight: 300;
-  }
-  input {
-    height: 30px;
-    margin: 10px 0;
-    font-size: 16px;
-    background: rgba(30, 39, 46, 0.2);
+  height: ${(props) => props.section};
+  input,
+  select {
+    height: 32px;
+    width: 100%;
+    // margin: 10px 0;
+    font-size: 14px;
+    // background: rgba(30, 39, 46, 0.2);
     border: none;
-    border-radius: 5px;
-    padding: 0 10px;
+    outline: none;
+    padding-top: 8px;
   }
 `;
 
-export const Button = styled.button`
-  height: 40px;
-  margin: 20px 0;
-  border-radius: 5px;
-  background: #222222;
-  color: rgba(189, 195, 199, 1);
-  cursor: pointer;
-  border: none;
-  outline: none;
+export const Box = styled.div`
+  width: 250px;
+  height: 60px;
+  border: 2px solid #e4e4e4;
+  border-radius: 8px;
+  padding: 4px 16px;
+  margin-bottom: 10px;
 
-  &:hover {
-    background: #3d3f43;
+  & > div {
+    flex: 1;
+  }
+  p {
+    font-size: 12px;
+  }
+`;
+
+export const Title = styled.div`
+
+    padding: 20px 24px;
+    height: 64px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    border-bottom: 2px solid #e4e4e4;
+
+    p {
+      font-size: 20px;
+      font-weight: 500;
+    }
   }
 `;
