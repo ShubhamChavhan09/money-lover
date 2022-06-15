7747212b70cf23d12c6a52d7f5a64570e721d4cf
Shubham <shubh.chavhan00@gmail.com>
2022-06-16 04:38:08 +0530

    data for expense categories

---
+++ src/category/index.js
@@ -0,0 +1,62 @@
+export const Category = [
+  {
+    name: "Food & Beveage",
+    id: 1,
+  },
+  {
+    name: "Bills & Utilities",
+    id: 2,
+  },
+  {
+    name: "Transportation",
+    id: 3,
+  },
+  {
+    name: "Shopping",
+    id: 4,
+  },
+  {
+    name: "Entertainment",
+    id: 5,
+  },
+  {
+    name: "Travel",
+    id: 6,
+  },
+  {
+    name: "Health & Fitness",
+    id: 7,
+  },
+  {
+    name: "Gift and Donations",
+    id: 8,
+  },
+  {
+    name: "Family",
+    id: 9,
+  },
+  {
+    name: "Education",
+    id: 10,
+  },
+  {
+    name: "Investment",
+    id: 11,
+  },
+  {
+    name: "Business",
+    id: 12,
+  },
+  {
+    name: "Insurance",
+    id: 13,
+  },
+  {
+    name: "Fees & Charges",
+    id: 14,
+  },
+  {
+    name: "Other Expense",
+    id: 15,
+  },
+];
