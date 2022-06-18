a6ff72d551e411ccfc711706aadd57385eaa89f4
Shubham <shubh.chavhan00@gmail.com>
2022-06-18 16:01:01 +0530

    changed layout for the buttons

--- src/components/modal-buttons/index.js
+++ src/components/modal-buttons/index.js
@@ -1,20 +1,20 @@
-import React from "react";
 import styled from "styled-components";
-import { Delete } from "../view-expense";
 
-const ModalButtons = ({ cancel }) => {
+const ModalButtons = ({ cancel, hidden }) => {
   return (
     <>
       <ButtonContainer>
-        <Button
-          onClick={cancel}
-          back="#f0f0f0"
-          color="#2EB84B"
-          hover="#2EB84B26"
-          type="button"
-        >
-          CANCEL
-        </Button>
+        {!hidden && (
+          <Button
+            onClick={cancel}
+            back="#f0f0f0"
+            color="#2EB84B"
+            hover="#2EB84B26"
+            type="button"
+          >
+            CANCEL
+          </Button>
+        )}
         <Button back="#2EB84B" color="#ffffff" hover="#2EB84BCC" type="submit">
           SAVE
         </Button>
@@ -27,21 +27,24 @@ export default ModalButtons;
 
 export const ButtonContainer = styled.div`
   height: 64px;
-  // border: 2px solid #e4e4e4;
   margin-top: 24px;
   text-align: right;
   padding: 14px 24px;
 `;
 
-export const Button = styled(Delete)`
+export const Button = styled.button`
+  border: none;
+  outline: none;
+  font-size: 14px;
+  width: 100px;
+  height: 36px;
+  cursor: pointer;
+  border-radius: 4px;
   color: ${(props) => props.color};
   background: ${(props) => props.back};
   margin-left: 18px;
 
   &:hover {
     background: ${(props) => props.hover};
-  }
-  &:disabled {
-    background: red;
   }
 `;
