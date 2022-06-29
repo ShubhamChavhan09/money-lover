652df23a7786c1da0732736398b9c3c54d408df4
Shubham <shubh.chavhan00@gmail.com>
2022-06-29 05:42:52 +0530

    added function to show expenses in the budget range

--- src/components/view-budget/index.js
+++ src/components/view-budget/index.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { CgClose } from "react-icons/cg";
-import { MISCELLANEOUS_BUDGET_ID, useBudgets } from "../../context";
+import { useBudgets } from "../../context";
 import { format } from "date-fns";
 import { currencyFormatter } from "../../utils";
 import EditBudgetModal from "../edit-budget-modal";
@@ -9,18 +9,86 @@ import ViewBudgetExpense from "../view-budget-expense";
 import TotalExpenseReport from "../total-expense-report";
 import { motion, AnimatePresence } from "framer-motion";
 import { IoArrowBack } from "react-icons/io5";
+import { useParams, useNavigate } from "react-router-dom";
+import DeleteModal from "../delete-modal";
+import { supabase } from "../../supabaseClient";
 
-const ViewBudget = ({
-  title,
-  budgetName,
-  toggle,
-  date,
-  budgetLeft,
-  des,
-  setDeleteModal,
-  startDate,
-  endDate,
-}) => {
+const ViewBudget = (
+  {
+    // title,
+    // budgetName,
+    // toggle,
+    // date,
+    // budgetLeft,
+    // des,
+    // setDeleteModal,
+    // startDate,
+    // endDate,
+  }
+) => {
+  let { budgetId } = useParams();
+  let navigate = useNavigate();
+
+  const { budgets, expenses, getBudgetExpenses, deleteBudget } = useBudgets();
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [budgetRangeData, setBudgetRangeData] = useState([]);
+
+  const singleBudget = budgets.filter((budget) => {
+    return budget.id === budgetId;
+  });
+
+  const budgetName = singleBudget[0]?.name;
+  const budgetMax = singleBudget[0]?.max;
+  const budgetDate = singleBudget[0]?.created;
+  const budgetStart = singleBudget[0]?.startDate;
+  const budgetEnd = singleBudget[0]?.endDate;
+
+  const daysLeft = new Date(budgetEnd).getDate() - new Date().getDate();
+
+  const daysData = () => {
+    if (daysLeft >= 0) {
+      return `${daysLeft} days left`;
+    } else {
+      return "Finished";
+    }
+  };
+
+  console.log({ daysLeft });
+  //
+  const dateString2 = budgetEnd && format(new Date(budgetEnd), "yyyy-MM-dd");
+  const dateString1 =
+    budgetStart && format(new Date(budgetStart), "yyyy-MM-dd");
+
+  useEffect(() => {
+    month();
+  }, []);
+
+  const month = async () => {
+    const { data, error } = await supabase
+      .from("expenses")
+      .select()
+      .lte("date", dateString2)
+      .gte("date", dateString1);
+    if (error) console.log("error", error);
+    else setBudgetRangeData(data);
+  };
+
+  const budgetData = budgetRangeData.filter((item) => {
+    return item.name === budgetName;
+  });
+
+  // //
+
+  const totalBudgetExpenseAmount = budgetData.reduce(
+    (total, expense) => total + expense.amount,
+    0
+  );
+  console.log({ budgetData });
+
+  const budgetAmountLeft = budgetMax - totalBudgetExpenseAmount;
+
+  //
+
   const [budgetModal, setBudgetModal] = useState(false);
   const [showBudgetExpense, setShowBudgetExpense] = useState(false);
 
@@ -65,8 +133,8 @@ const ViewBudget = ({
       >
         <Head>
           <div>
-            <Close onClick={() => toggle(false)} />
-            <p>{title}</p>
+            <Close onClick={() => navigate("/")} />
+            <p>Detail</p>
           </div>
           <div>
             <Delete onClick={handleDelete}>DELETE</Delete>
@@ -75,36 +143,41 @@ const ViewBudget = ({
         </Head>
         <Details>
           <h3>{budgetName}</h3>
-          <p>{format(new Date(date), "EEEE, dd/MM/yy")}</p>
+          {/* <p>{budgetDate && format(new Date(budgetDate), "EEEE, dd/MM/yy")}</p> */}
+          <p>{daysData()}</p>
           <hr />
-          <p>{des}</p>
-          <h3>{currencyFormatter.format(Math.abs(budgetLeft))}</h3>
+          <p>{budgetAmountLeft >= 0 ? "Left" : "Overspent"}</p>
+          <h3>{currencyFormatter.format(Math.abs(budgetAmountLeft))}</h3>
         </Details>
         <Chart>
-          <TotalExpenseReport
-            date={date}
-            name={budgetName}
-            startDate={startDate}
-            endDate={endDate}
-          />
+          <TotalExpenseReport chartData={budgetData} />
         </Chart>
         <View>
           <p onClick={handleModal}>VIEW TRANSACTION</p>
         </View>
       </Container>
+
       <EditBudgetModal
         budgetModal={budgetModal}
         setBudgetModal={setBudgetModal}
         name={budgetName}
-        toggleViewBudget={toggle}
-        startDate={startDate}
-        endDate={endDate}
+        startDate={budgetStart}
+        endDate={budgetEnd}
+        id={budgetId}
       />
       <ViewBudgetExpense
         showBudgetExpense={showBudgetExpense}
         close={setShowBudgetExpense}
-        name={budgetName}
-        // name={budgetName[0] ? budgetName[0]?.name : "Miscellaneous"}
+        data={budgetData}
+        total={totalBudgetExpenseAmount}
+      />
+      <DeleteModal
+        deleteModal={deleteModal}
+        toggle={setDeleteModal}
+        deleteId={budgetId}
+        redirect={"/"}
+        func={deleteBudget}
+        alert="Do you want to delete this budget?"
       />
     </>
   );
@@ -115,12 +188,15 @@ export default ViewBudget;
 const Container = styled(motion.div)`
   background: #ffffff;
   width: min(92vw, 600px);
-  height: 600px;
+  height: min(100%, 620px);
   border-radius: 4px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   box-shadow: 0 3px 7px 0 rgb(0 0 0 / 27%);
+  margin: auto;
+  background: linear-gradient(145deg, #f5f6fa, #ffffff);
+  box-shadow: 27px 27px 54px #666666, -27px -27px 54px #ffffff;
 `;
 const Head = styled.div`
   height: 64px;
