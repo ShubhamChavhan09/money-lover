2a93774cec4045da01182aeb37481ee746eeb102
Shubham <shubh.chavhan00@gmail.com>
2022-07-02 01:49:04 +0530

    added new Routes for viewing budgets and expenses on differnt routes

--- src/router-files/index.js
+++ src/router-files/index.js
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Expenses from "../pages/Expenses";
 import Report from "../pages/Report";
 import Dashboard from "../pages/Budgets";
+import ViewBudget from "../components/view-budget";
+import ViewExpense from "../components/view-expense";
 
 const RouterFiles = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/:budgetId" element={<ViewBudget />} />
       <Route path="/expenses" element={<Expenses />} />
+      <Route path="/expenses/:expenseId" element={<ViewExpense />} />
       <Route path="/report" element={<Report />} />
     </Routes>
   );
