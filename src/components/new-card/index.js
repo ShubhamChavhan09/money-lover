c46824178335a1fe4358930003f6d50aee847b59
Shubham <shubh.chavhan00@gmail.com>
2022-07-01 06:50:35 +0530

    moved all the code to one step up to the parent

--- src/components/new-card/index.js
+++ src/components/new-card/index.js
@@ -1,69 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useBudgets } from "../../context";
 import BudgetCard from "../budget-card";
 import { v4 as uuidv4 } from "uuid";
 import { motion } from "framer-motion";
+import { supabase } from "../../supabaseClient";
 
-const NewCard = ({
-  setViewBudgetTab,
-  toggleState,
-  setBudgetName,
-  setSelectedBudgetId,
-}) => {
-  const { budgets, getBudgetExpenses } = useBudgets();
-
-  // const data = budgets.sort((a, b) => {
-  //   return new Date(b.created) - new Date(a.created);
-  // });
-
-  // const filteredData = data.filter((budget) => {
-  //   if (inputText === "") {
-  //     return budget;
-  //   } else {
-  //     return budget.name.toLowerCase().includes(inputText);
-  //   }
-  // });
-
-  let today = new Date();
-  const runningBudgets = budgets
-    .map((budget) => {
-      return budget;
-    })
-    .filter((item) => {
-      return (
-        new Date(item.startDate) <= today && new Date(item.endDate) >= today
-      );
-    });
-
-  const finishedBudgets = budgets
-    .map((budget) => {
-      return budget;
-    })
-    .filter((item) => {
-      return (
-        new Date(item.startDate) >= today || new Date(item.endDate) <= today
-      );
-    });
-
-  const dataBud = toggleState === 1 ? runningBudgets : finishedBudgets;
+const NewCard = ({ toggleState, data }) => {
+  const { getBudgetExpenses } = useBudgets();
 
   return (
     <Budget>
-      {dataBud.map((budget) => {
+      {data.map((budget) => {
         const amount = getBudgetExpenses(budget.name).reduce(
           (total, expense) => total + expense.amount,
           0
         );
         return (
           <div key={uuidv4()}>
-            <BudgetCard
-              amount={amount}
-              budget={budget}
-              setBudgetName={setBudgetName}
-              setViewBudgetTab={setViewBudgetTab}
-              setSelectedBudgetId={setSelectedBudgetId}
-            />
+            <BudgetCard amount={amount} budget={budget} />
           </div>
         );
       })}
