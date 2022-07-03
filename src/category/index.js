f2a40a3379c017c81f808295315bdb4e2613876c
Shubham <shubh.chavhan00@gmail.com>
2022-07-03 21:55:13 +0530

    :bug: fix(typo error)

--- src/category/index.js
+++ src/category/index.js
@@ -2,61 +2,76 @@ export const Category = [
   {
     name: "Food & Beveage",
     id: 1,
+    url: "https://static.moneylover.me/img/icon/ic_category_foodndrink.png",
   },
   {
     name: "Bills & Utilities",
     id: 2,
+    url: "https://static.moneylover.me/img/icon/icon_135.png",
   },
   {
     name: "Transportation",
     id: 3,
+    url: "https://static.moneylover.me/img/icon/ic_category_transport.png",
   },
   {
     name: "Shopping",
     id: 4,
+    url: "https://static.moneylover.me/img/icon/ic_category_shopping.png",
   },
   {
     name: "Entertainment",
     id: 5,
+    url: "https://static.moneylover.me/img/icon/ic_category_entertainment.png",
   },
   {
     name: "Travel",
     id: 6,
+    url: "https://static.moneylover.me/img/icon/ic_category_travel.png",
   },
   {
     name: "Health & Fitness",
     id: 7,
+    url: "https://static.moneylover.me/img/icon/ic_category_medical.png",
   },
   {
     name: "Gift and Donations",
     id: 8,
+    url: "https://static.moneylover.me/img/icon/ic_category_donations.png",
   },
   {
     name: "Family",
     id: 9,
+    url: "https://static.moneylover.me/img/icon/ic_category_family.png",
   },
   {
     name: "Education",
     id: 10,
+    url: "https://static.moneylover.me/img/icon/ic_category_education.png",
   },
   {
     name: "Investment",
     id: 11,
+    url: "https://static.moneylover.me/img/icon/ic_category_invest.png",
   },
   {
     name: "Business",
     id: 12,
+    url: "https://static.moneylover.me/img/icon/icon_59.png",
   },
   {
     name: "Insurance",
     id: 13,
+    url: "https://static.moneylover.me/img/icon/icon_137.png",
   },
   {
     name: "Fees & Charges",
     id: 14,
+    url: "https://static.moneylover.me/img/icon/icon_138.png",
   },
   {
     name: "Other Expense",
     id: 15,
+    url: "https://static.moneylover.me/img/icon/ic_category_other_expense.png",
   },
 ];
